2b93e4a8241e695fb29b735dc77f98e5e31e304c
heeyoung35 <kheeyoung35@gmail.com>
2025-11-03 18:53:21 +0900

    Add files via upload

--- 05_java/chap07-object-array/src/com/ohgiraffers/section01/init/Application.java
+++ 05_java/chap07-object-array/src/com/ohgiraffers/section01/init/Application.java
@@ -5,33 +5,42 @@ import javax.swing.text.html.CSS;
 public class Application {
     public static void main(String[] args) {
 
+        // 자동차 객체 생성 매개변수 있는 생성자만 있으면 기본생성자 호출 불가
+        // 생성자가 하나도 없으면 기본 생성자를 컴파일러가 만들어 주지만 생성자를 하나라도 만들면
+        // 컴파일러가 기본 생성자를 만들어 주지 않는다. 지금은 매개변수 있는 생성자가 있기 때문에 기본생성자를 만들어주지 않음
+        // new Car 객체를 생성해서 heap영역에 새로운 객체 생성 static에 주소값 참조
         Car car1 = new Car("페라리", 300);
         Car car2 = new Car("람보르기니", 350);
         Car car3 = new Car("롤스로이스", 250);
         Car car4 = new Car("부가티베이론", 400);
         Car car5 = new Car("포터", 200);
 
+        // 래퍼선스 변수로 호출
         car1.driveMaxSpeed();
         car2.driveMaxSpeed();
         car3.driveMaxSpeed();
         car4.driveMaxSpeed();
         car5.driveMaxSpeed();
+        // 위에처럼 반복적이고 비효울적 작업을 위해서 레퍼런스 변수를 배열에 담아볼 예정이다.
+
 
         /* 객체배열은 레퍼런스 변수들을 보관하는 배열이다.
         *  동일한 타입의 여러 객체들을 하난의 묶음으로 관리하여 연속적으로 처리할 때 유용하다.
         * */
-        Car[] carArray = new Car[5];
+        Car[] carArray = new Car[5];  // 5칸의 배열만 만든상태
 
         /* 배열을 선언만 하면 기본값으로 초기화된다.
         *  기본자료형(int, double 등)은 0, 0.0으로 초기화 되지만,
         *  참조자료형(Car, String 등)의 기본값은 null 이다.
         * */
-        System.out.println(carArray[0]);
+        System.out.println(carArray[0]); // 출력값 null
+        // 위에처럼 배열만 생성하고 참조하지 않은 배열에 무엇을 하겠다고 하면 NullPointerException발생하고
+        // 프로그램이 종료된다. 그래서 아래처럼 try catch문을 이용해서 Exception발생시 실행하는 코드 작성으로 프로그램종료 예방
 
         try {
             carArray[0].driveMaxSpeed();
-        } catch (NullPointerException e) {
-            System.out.println("!! NullPointerException발생!!");
+        } catch (NullPointerException e) { // NullPointerException도 자료형이기 때문에 e를 넣어준다.
+            System.out.println("!!! NullPointerException발생!!");
         }
 
         carArray[0] = new Car("페라리", 300);
--- 05_java/chap07-object-array/src/com/ohgiraffers/section01/init/Car.java
+++ 05_java/chap07-object-array/src/com/ohgiraffers/section01/init/Car.java
@@ -1,12 +1,13 @@
 package com.ohgiraffers.section01.init;
 
 public class Car {
-
-    // 필드 (자동차의 속성)
+    // 필드를 private으로 캡슐화를 해주고 있다. 외부에서 이필드에 직접 접근 불가
+    // 필드 (자동차의 속성) 모델 이름과 최대 스피드의 속성
     private String modelName;
     private int maxSpeed;
 
     // 생섲자 (자동차를 처음 만들 때 필요한 값을 받아 초기화)
+    // 매개변수 있는 생성자로 클래스 자료형, 이름과 동일하게 해야 생성자 형태
     public Car(String modelName, int maxSpeed) {
         this.modelName = modelName;
         this.maxSpeed = maxSpeed;
@@ -17,6 +18,7 @@ public class Car {
         System.out.println(this.modelName + "이(가) 최고시속" + this.maxSpeed + "km/h로 달려갑니다.");
     }
 
+    // 캡슐화로 인해 접근할 수 있도록 값을 읽어오는 getter
     public String getModelName() {
         return modelName;
     }
