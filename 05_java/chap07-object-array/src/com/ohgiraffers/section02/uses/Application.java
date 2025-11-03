e1319d4b2fb8da6116f81aa916dd8b658c683e87
heeyoung35 <kheeyoung35@gmail.com>
2025-11-03 17:55:05 +0900

    Add files via upload

---
+++ 05_java/chap07-object-array/src/com/ohgiraffers/section02/uses/Application.java
@@ -0,0 +1,40 @@
+package com.ohgiraffers.section02.uses;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+
+        /*
+        * 요구사항
+        * 1. 여러 명의 회원 정보를 받아 한 번에 여러 회원 정보를 등록
+        * 2. 전체 히원 조회 시 여러 명의 회원 종보를 반환
+        * */
+
+        Scanner sc = new Scanner(System.in);
+
+        MemberService memberService = new MemberService();
+
+        while (true) {
+            System.out.println("======회원 관리 프로그램=====");
+            System.out.println("1. 회원 등록");
+            System.out.println("2. 회원 전체 조회");
+            System.out.println("3. 회원 아이디로 검색");
+            System.out.println("3. 프로그램 종료");
+            System.out.println("메뉴 선택");
+            int no = sc.nextInt();
+
+            switch (no) {
+                case 1 : memberService.signUpMembers(); break;
+                case 2 : memberService.showAllMEmbers(); break;
+                case 3 : memberService.searchMemberById(); break;
+                case 9 :
+                    System.out.println("프로그램을 종료합니다");
+                    sc.close(); // Scanner 자원 반납
+                    return;
+                default:
+                    System.out.println("잘못된 번호를 입력하셨습니다."); break;
+            }
+        }
+    }
+}
